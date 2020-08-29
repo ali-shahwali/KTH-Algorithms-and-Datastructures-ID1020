5c831d3d6d8b84185e7f87145ce8829dba6894bf
Ali Shahwali <ali_zinq@hotmail.com>
2020-08-29 21:44:06 +0200

    Add files via upload

--- Labb1_task3/DoublyLinkedIterable.java
+++ Labb1_task3/DoublyLinkedIterable.java
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 package DoubleCircLinkedList;
 
 public interface DoublyLinkedIterable<Item>
 {
     public DoublyLinkedIterator<Item> DoublyLinkedIterator();
 }
-=======
-package DoubleCircLinkedList;
-
-public interface DoublyLinkedIterable<Item>
-{
-    public DoublyLinkedIterator<Item> DoublyLinkedIterator();
-}
->>>>>>> 2a333ce8ce4414817ecbc193e0d0c09bccafe095
--- Labb1_task3/DoublyLinkedIterator.java
+++ Labb1_task3/DoublyLinkedIterator.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package DoubleCircLinkedList;
 
 public interface DoublyLinkedIterator<Item>
@@ -8,14 +7,3 @@ public interface DoublyLinkedIterator<Item>
     public Item prev();
     public Item next();
 }
-=======
-package DoubleCircLinkedList;
-
-public interface DoublyLinkedIterator<Item>
-{
-    public boolean hasNext();
-    public boolean hasPrev();
-    public Item prev();
-    public Item next();
-}
->>>>>>> 2a333ce8ce4414817ecbc193e0d0c09bccafe095
