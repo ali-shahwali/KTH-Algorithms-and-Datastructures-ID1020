5b89ec1b568bb3d6999f4f2f3f7981fbf15770d2
Ali Shahwali <ali_zinq@hotmail.com>
2020-08-29 22:20:59 +0200

    Delete DoublyLinkedIterator.java

--- Labb1_task3/DoublyLinkedIterator.java
+++
@@ -1,9 +0,0 @@
-package DoubleCircLinkedList;
-
-public interface DoublyLinkedIterator<Item>
-{
-    public boolean hasNext();
-    public boolean hasPrev();
-    public Item prev();
-    public Item next();
-}
