fe348443b96c44b11ffe37c579b5185bb95dd90f
Ali Shahwali <ali_zinq@hotmail.com>
2020-08-31 17:25:22 +0200

    Add files via upload

---
+++ Labb1_task4/CircLinkedListTest.java
@@ -0,0 +1,9 @@
+package Labb1_task4;
+
+public class CircLinkedListTest
+{
+    public static void main(String[] args)
+    {
+        CircLinkedList<String> queue = new CircLinkedList<String>();
+    }
+}
