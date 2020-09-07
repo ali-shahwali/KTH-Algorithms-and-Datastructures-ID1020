a5835e970a55d54df328ed0b9d4ad22ad3f6ed11
xdmeister <ali_zinq@hotmail.com>
2020-09-07 16:45:20 +0200

    added comments to task 1

--- src/Labb1_task1/ReverseText.c
+++ src/Labb1_task1/ReverseText.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+/*
+we create a char 'ch' that takes input from the getchar function, as long as ch != a newline we call upon the function again
+and effectively create a new instance of the method, as soon as we hit newline we go through each instance of the method and
+putchar is used to display the the given characters on stdout, because we created a new instance through recursion we will
+therefor get the sequence of characters in reverse order when jumping back in every instance
+*/
 void recursiveReverse()
 {
     char ch;
@@ -9,7 +15,12 @@ void recursiveReverse()
     }
     putchar(ch);
 }
-
+/*
+uses malloc to provide allocate space for 50 characters, uses getchar and stores the given input in to a char array 'str'
+we track the length of the array with the variable 'len' and in the first while loop we repeat this process and move
+'str's adress foward until a newline is fed in. then we enter the next while loop and use putchar to place each char in 'str' 
+onto stdout and move the address backward until 'len' is 0.
+*/
 void iterativeReverse()
 {
     char* str = (char*)malloc(50*sizeof(char));
