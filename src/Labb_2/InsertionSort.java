449e64e17eb8274987bd95a8d30c60e03ce4888b
Ali Shahwali <ali_zinq@hotmail.com>
2020-09-11 22:46:40 +0200

    started lab 2 task 1

--- src/Labb_2/InsertionSort.java
+++ src/Labb_2/InsertionSort.java
@@ -1,4 +1,34 @@
 package Labb_2;
 
-public class InsertionSort {
+import java.util.Arrays;
+
+public class InsertionSort
+{
+    public static void sort(int[] arr)
+    {
+        int swaps = 0;
+        for(int i = 0; i < arr.length;i++)
+        {
+            for(int j = i; j > 0 && less(arr[j],arr[j-1]);j--)
+            {
+                exchange(arr, j, j-1);
+                swaps++;
+                System.out.println(Arrays.toString(arr));
+            }
+        }
+        System.out.println(swaps);
+    }
+
+    private static void exchange(int[] arr, int i, int j)
+    {
+        int temp = i;
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    private static boolean less(int i, int j)
+    {
+        return i < j;
+    }
 }
+
--- src/Labb_2/InsertionSortTest.java
+++ src/Labb_2/InsertionSortTest.java
@@ -1,4 +1,26 @@
 package Labb_2;
+import java.util.Arrays;
+import java.util.Scanner;
+public class InsertionSortTest
+{
+    public static void main(String[] args)
+    {
+        Scanner intScan = new Scanner(System.in);
+        System.out.println("Enter how many integers you want to sort");
+        int n = intScan.nextInt();
+        int[] arr = new int[n];
 
-public class InsertionSortTest {
+        System.out.println("Enter what you want to sort");
+        for(int i = 0; i < arr.length; i++)
+        {
+            n = intScan.nextInt();
+            arr[i] = n;
+            System.out.println(Arrays.toString(arr));
+        }
+
+        System.out.println("Sorting...");
+
+        InsertionSort.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
 }
