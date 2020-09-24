2f01828d0422a5bb9d2c1333c3c5f107b5cbc284
xdmeister <ali_zinq@hotmail.com>
2020-09-24 15:12:28 +0200

    added time comparison for task 2

--- src/Labb_3/Task2/FrequencyCounterBST.java
+++ src/Labb_3/Task2/FrequencyCounterBST.java
@@ -6,17 +6,17 @@ public class FrequencyCounterBST
 {
     public static void main(String[] args) throws FileNotFoundException
     {
-        File theText = new File("C:\\Users\\ali_z\\IdeaProjects\\KTH-Algoritmer-Datastrukturer-ID1020\\src\\Labb_3\\TheText.txt");
+        File theText = new File("C:\\Users\\ali_z\\IdeaProjects\\KTH-Algoritmer-Datastrukturer-ID1020\\src\\Labb_3\\Task1\\TheText.txt");
         Scanner reader = new Scanner(theText);
         Scanner input = new Scanner(System.in);
         System.out.println("How many words do you want to read? Enter now: ");
         int n = input.nextInt();
 
         BinarySearchTree<String, Integer> bst = new BinarySearchTree<String, Integer>();
-        int iterations = 0, distinct = 0;
-        int minLen = 3;
+        int iterations = 0, distinct = 0, minLen = 3;
         String key, mostFreq = "O";
         bst.put(mostFreq,0);
+        long startTime = System.nanoTime();
         while(reader.hasNextLine() && iterations < n)
         {
             key = reader.next();
@@ -34,7 +34,8 @@ public class FrequencyCounterBST
 
             iterations++;
         }
-
+        long finishTime = System.nanoTime();
+        System.out.println("Time elapsed: " + (double)(finishTime-startTime)/1000000000);
         System.out.println("Number of words " + iterations);
         System.out.println("Number of distinct words " + distinct);
         System.out.println("Most frequent word '" + mostFreq + "' it appeared " + bst.get(mostFreq));
--- src/Labb_3/Task2/FrequencyCounterOrdArrST.java
+++ src/Labb_3/Task2/FrequencyCounterOrdArrST.java
@@ -1,6 +1,6 @@
 package Labb_3.Task2;
 import java.io.FileNotFoundException;
-import java.util.Iterator;
+//import java.util.Iterator;
 import java.util.Scanner;
 import java.io.File;
 public class FrequencyCounterOrdArrST
@@ -8,7 +8,7 @@ public class FrequencyCounterOrdArrST
     public static void main(String[] args) throws FileNotFoundException
     {
         int minlen = 3;
-        File theText = new File("C:\\Users\\ali_z\\IdeaProjects\\KTH-Algoritmer-Datastrukturer-ID1020\\src\\Labb_3\\TheText.txt");
+        File theText = new File("C:\\Users\\ali_z\\IdeaProjects\\KTH-Algoritmer-Datastrukturer-ID1020\\src\\Labb_3\\Task1\\TheText.txt");
         Scanner reader = new Scanner(theText);
         Scanner input = new Scanner(System.in);
         System.out.println("How many words do you want to read? Enter now: ");
@@ -17,7 +17,9 @@ public class FrequencyCounterOrdArrST
         OrderedArrayST<String, Integer> st = new OrderedArrayST<String, Integer>(n);
         int iterations = 0, distinct = 0;
 
-        String key;
+        String key, mostFreq = "";
+        st.put(mostFreq,0);
+        long startTime = System.nanoTime();
         while(reader.hasNextLine() && iterations < n)
         {
             key = reader.next();
@@ -32,9 +34,13 @@ public class FrequencyCounterOrdArrST
             else
                 st.put(key,st.get(key) + 1);
 
+            if(st.get(key) > st.get(mostFreq))
+                mostFreq = key;
+
             iterations++;
         }
-
+        long finishTime = System.nanoTime();
+        /*
         Iterator itr = st.iterator();
 
         String maxKey = st.min();
@@ -49,9 +55,10 @@ public class FrequencyCounterOrdArrST
                 maxValue = st.get(current);
             }
         }
-
-        System.out.println("Most frequent word was '" + maxKey + "'," + " it appeared "
-                + maxValue + " times.");
+        */
+        System.out.println("Time elapsed: " + (double)(finishTime-startTime)/1000000000);
+        System.out.println("Most frequent word was '" + mostFreq + "'," + " it appeared "
+                + st.get(mostFreq) + " times.");
         System.out.println("Nr of distinct words: " + distinct);
         System.out.println("Number of words: " + iterations);
     }
